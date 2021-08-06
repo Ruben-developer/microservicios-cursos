1c13d9bb0abe2f7e64641dc86897a4688088760a
Ruben-developer <rcontreras.dev@protonmail.com>
2021-08-06 13:45:29 -0400

    Uso de Query con join para obtener el curso asignado a un alumno

--- microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/services/CursoService.java
+++ microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/services/CursoService.java
@@ -5,4 +5,5 @@ import com.formaciondbi.microservicios.commons.services.CommonService;
 
 public interface CursoService extends CommonService<Curso> {
 
+	public Curso findCursoByAlumnoId(Long id);
 }
